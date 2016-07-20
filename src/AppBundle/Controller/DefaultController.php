16e0403cbdf0140b2bc3e5219ec296530937ef8d
Guobin <guobin.qiu@d8aspring.com>
2016-07-20 15:33:04 +0800

    refactoring

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,12 +13,21 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/", name="header")
+     */
+    public function headerAction(Request $request)
+    {
         $current_user = null;
 
         $id = $request->getSession()->get('id');
         if ($id != null) {
             $current_user = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
         }
-        return $this->render('default/index.html.twig', array('current_user' => $current_user));
+
+        return $this->render('default/header.html.twig', array('current_user' => $current_user));
     }
 }
--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/users")
@@ -13,8 +14,17 @@ class UserController extends Controller
     /**
      * @Route("/login", name="users_login", methods={"GET"})
      */
-    public function LoginAction()
+    public function loginAction()
     {
         return $this->render('user/login.html.twig');
+    }
+
+    /**
+     * @Route("/logout", name="users_logout", methods={"DELETE"})
+     */
+    public function logoutAction(Request $request)
+    {
+        $request->getSession()->set('id', null);
+        return $this->redirect($this->generateUrl('homepage'));
     }
 }
